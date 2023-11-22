19e08dcf96b6e7c59143aac727d421306b8cd9a0
Gabriel Penteado <gabripenteado@gmail.com>
2023-11-21 21:44:02 -0300

    created Tasks component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import "./App.css";
-import Counter from "./Counter";
+import Counter from "./components/Counter";
+import Tasks from "./components/Tasks";
 
 function App() {
   return (
-    <>
+    <div className="container">
       <Counter initialCount={0} color="black" />
-    </>
+      <Tasks />
+    </div>
   );
 }
 
---
+++ src/components/Tasks.tsx
@@ -0,0 +1,32 @@
+import { useState } from "react";
+import axios from "axios";
+import Button from "./Button";
+
+interface ITasks {
+  id: string;
+  title: string;
+}
+
+const Tasks = () => {
+  const [tasks, setTasks] = useState<ITasks[]>([]);
+
+  const handleClick = async () => {
+    const { data } = await axios.get(
+      "https://jsonplaceholder.typicode.com/todos?_limit=10"
+    );
+    setTasks(data);
+  };
+
+  return (
+    <div className="tasks">
+      <h1>Tasks from API</h1>
+      <Button onClick={handleClick}>Get Tasks from API</Button>
+
+      {tasks.map((tasks) => (
+        <p id={tasks.id}>{tasks.title}</p>
+      ))}
+    </div>
+  );
+};
+
+export default Tasks;
